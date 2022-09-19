350c03c01c34adccbadb2a1b6b22b28a65393f1a
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-09-19 23:51:23 +0300

    add devTools

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import { GlobalStyle } from './components/GlobalStyle';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
