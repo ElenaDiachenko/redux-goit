75980ba32b855028472fac4a1dc4778cc8dea01c
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-09-20 00:21:16 +0300

    update

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,20 @@
 import { FilterButton } from 'components/FilterButton/FilterButton';
 import css from './StatusFilter.module.css';
+import { useSelector } from 'react-redux';
+import { statusFilters } from 'redux/constants';
 
 export const StatusFilter = () => {
+  const filter = useSelector(state => state.filters.status);
+
   return (
     <div className={css.wrapper}>
-      <FilterButton>All</FilterButton>
-      <FilterButton>Active</FilterButton>
-      <FilterButton>Completed</FilterButton>
+      <FilterButton selected={filter === statusFilters.all}>All</FilterButton>
+      <FilterButton selected={filter === statusFilters.active}>
+        Active
+      </FilterButton>
+      <FilterButton selected={filter === statusFilters.completed}>
+        Completed
+      </FilterButton>
     </div>
   );
 };
--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -1,6 +1,9 @@
 import css from './TaskCounter.module.css';
+import { useSelector } from 'react-redux';
 
 export const TaskCounter = () => {
+  const tasks = useSelector(state => state.tasks);
+
   return (
     <div>
       <p className={css.text}>Active: 0</p>
