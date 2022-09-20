de9bdf901251d0a47ff4c187d49d113f6908c731
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-09-20 15:18:15 +0300

    done

--- src/components/Actions/Actions.jsx
+++ src/components/Actions/Actions.jsx
@@ -1,11 +1,18 @@
 import { FilterButton } from 'components/FilterButton/FilterButton';
 import { useDispatch } from 'react-redux';
+import { toggleAllCompleted, deleteAllCompleted } from 'redux/actions';
 
 export const Actions = () => {
+  const dispatch = useDispatch();
+
   return (
     <div style={{ display: 'flex', gap: 4 }}>
-      <FilterButton>Mark all completed</FilterButton>
-      <FilterButton>Clear all completed</FilterButton>
+      <FilterButton onClick={() => dispatch(toggleAllCompleted())}>
+        Mark all completed
+      </FilterButton>
+      <FilterButton onClick={() => dispatch(deleteAllCompleted())}>
+        Clear all completed
+      </FilterButton>
     </div>
   );
 };
