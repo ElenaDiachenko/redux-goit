48969952a31eebc6bb883278d0749f5be87b3ce1
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-09-20 14:24:18 +0300

    update reducer

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,18 +1,33 @@
 import { FilterButton } from 'components/FilterButton/FilterButton';
-import css from './StatusFilter.module.css';
-import { useSelector } from 'react-redux';
+// import css from './StatusFilter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 import { statusFilters } from 'redux/constants';
+import { getFilter } from 'redux/selectors';
+import { setStatusFilter } from 'redux/actions';
 
 export const StatusFilter = () => {
-  const filter = useSelector(state => state.filters.status);
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+  const handleChange = filter => dispatch(setStatusFilter(filter));
 
   return (
-    <div className={css.wrapper}>
-      <FilterButton selected={filter === statusFilters.all}>All</FilterButton>
-      <FilterButton selected={filter === statusFilters.active}>
+    <div style={{ display: 'flex', gap: 4 }}>
+      <FilterButton
+        selected={filter === statusFilters.all}
+        onClick={() => handleChange(statusFilters.all)}
+      >
+        All
+      </FilterButton>
+      <FilterButton
+        selected={filter === statusFilters.active}
+        onClick={() => handleChange(statusFilters.active)}
+      >
         Active
       </FilterButton>
-      <FilterButton selected={filter === statusFilters.completed}>
+      <FilterButton
+        selected={filter === statusFilters.completed}
+        onClick={() => handleChange(statusFilters.completed)}
+      >
         Completed
       </FilterButton>
     </div>
--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,15 @@
+import { useDispatch } from 'react-redux';
 import { FilterButton } from 'components/FilterButton/FilterButton';
 import css from './TaskForm.module.css';
+import { addTask } from 'redux/actions';
 
 export const TaskForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
+    dispatch(addTask(form.elements.text.value));
     form.reset();
   };
 
